b19fd5a84f2107a416db6db8aa75f990040f4a93
唐道源 <tangdaoyuan@megvii.com>
2022-04-22 20:50:15 +0800

    feat: intercept `acess` and  `stat`

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,35 @@ _internalFs.open = function(...args: any[]) {
   _onOpen(args[0])
   return _open(...args)
 }
+
+const _access = _internalFs.access
+_internalFs.access = function(...args: any[]) {
+  _onOpen(args[0])
+  return _access(...args)
+}
+
+const _stat = _internalFs.stat
+_internalFs.stat = function(...args: any[]) {
+  _onOpen(args[0])
+  return _stat(...args)
+}
+
+const _lstat = _internalFs.lstat
+_internalFs.lstat = function(...args: any[]) {
+  _onOpen(args[0])
+  return _lstat(...args)
+}
+
 const _openFileHandle = _internalFs.openFileHandle
 _internalFs.openFileHandle = function(...args: any[]) {
   _onOpen(args[0])
   return _openFileHandle(...args)
+}
+
+const _FileHandle = _internalFs.FileHandle
+_internalFs.FileHandle = function(...args: any[]) {
+  _onOpen(args[0])
+  return _FileHandle(...args)
 }
 
 const _listeners: OnFileOpenListener[] = []
